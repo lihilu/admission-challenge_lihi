471f60d2a5fe1be16603d503a7d961f452f3f59d
lihilu <99220011+lihilu@users.noreply.github.com>
2022-02-11 00:01:08 +0200

    Add files via upload

--- materials/vagrant/fixScripts/exercise5-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 #add fix to exercise5-server1 here
+rm /home/vagrant/.ssh/id_rsa*
+ssh-keygen -N '' -f /home/vagrant/.ssh/id_rsa -C vagrant
+chown vagrant:vagrant /home/vagrant/.ssh/id_rsa*
+cp /home/vagrant/.ssh/id_rsa.pub /vagrant/
+echo '   StrictHostKeyChecking no' >> /etc/ssh/ssh_config
+
--- materials/vagrant/fixScripts/exercise6-fix.sh
+++ materials/vagrant/fixScripts/exercise6-fix.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 #add fix to exercise6-fix here
+cp $@
+scp -r "${@: -1}" server2:/
+du -bc "${@: -1}" | awk '{print $1}' | tail -1
